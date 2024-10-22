84770f95c74451e0e5ceac018cafc07ccb5e538a
Daniel Komesu <danielkomesu@gmail.com>
2024-10-22 15:41:02 -0300

    Update script to process datasus's dbc files

--- codigo/1-process-dbc-files.R
+++ codigo/1-process-dbc-files.R
@@ -1,20 +1,40 @@
 # Conversão dos arquivos "DBC" do DATASUS para parquet
 
+# Para instalar os pacotes necessários, execute:
+
+# install.packages("arrow")
+# install.packages("dplyr")
+# install.packages("fs")
+# install.packages("read.dbc")
+# install.packages("tibble")
+
 # Carregando bibliotecas
 library(arrow)
 library(dplyr)
+library(fs)
 library(read.dbc)
+library(tibble)
 
 
-data_dir <- "output"
+data_dir <- "C:\\data\\datasus"
+dest_dir <- "C:\\data\\datasus\\parquet"
+dir_create(dest_dir)
 
 # Lista de arquivos DBC
-files <- list.files(data_dir, pattern = ".dbc$", full.names = TRUE)
+files <- dir_ls(
+  data_dir,
+  recurse = TRUE,
+  glob = "*.dbc"
+)
 
-# Função para converter DBC para parquet
-convert_dbc_to_parquet <- function(file) {
-  # Lendo DBC
-  df <- read.dbc(file)
-  df
+dados <- tibble()
+for (file in files) {
+  print(paste("Lendo arquivo:", file))
+  # Lendo arquivo DBC
+  d <- read.dbc(file, as.is = TRUE) |> as_tibble()
+  dados <- bind_rows(dados, d)
+  rm(d)  # Liberando memória
+  gc()  # Coleta de lixo
 }
 
+write_parquet(dados, path(dest_dir, "sinan-deng_2020-2024.parquet"))
