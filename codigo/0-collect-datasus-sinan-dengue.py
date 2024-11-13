060fe47cd294ecb2084525931e0b5fcb7b4c1dc0
Daniel Komesu <danielkomesu@gmail.com>
2024-11-13 14:50:03 -0300

    Update script 0-collect-datasus-sinan-dengue

--- codigo/0-collect-datasus-sinan-dengue.py
+++ codigo/0-collect-datasus-sinan-dengue.py
@@ -10,14 +10,20 @@ from pathlib import Path
 from datasus_fetcher import fetcher
 from datasus_fetcher.slicer import Slicer
 
-# Definindo o diretório de saída
-output_dir = Path("C:\\data\\datasus")
 
-# Coletando os dados do SINAN Dengue
-fetcher.download_data(
-    datasets=["sinan-deng", "sinan-deng-preliminar"],
-    destdir=output_dir,
-    threads=1,
-    slicer=Slicer(start_time="2020", end_time="2024"),
-    callback=print,
-)
+def main():
+    # Definindo o diretório de saída
+    output_dir = Path("C:\\data\\datasus")
+
+    # Coletando os dados do SINAN Dengue
+    fetcher.download_data(
+        datasets=["sinan-deng", "sinan-deng-preliminar"],
+        destdir=output_dir,
+        threads=1,
+        slicer=Slicer(start_time="2020", end_time="2024"),
+        callback=print,
+    )
+
+
+if __name__ == "__main__":
+    main()
